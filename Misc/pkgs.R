53dd109a6321d12a91e1b9dcb1b0ff6f30cfcf4c
Savet Hong <savet3@gmail.com>
2016-10-19 22:39:00 -0700

    create list of useful R library packages

---
+++ Misc/pkgs.R
@@ -0,0 +1,17 @@
+# Useful Packages to know or have
+
+#loading various forms of data
+read.pkgs <- c("readstata13", # STATA version 13 files
+              "readxl" )     # read excel files
+
+#data cleaning/processing
+cln.pkgs <- c("dplyr")
+
+#data visualization
+vis.pkgs <- c("flexdashboard")
+
+#Graphics
+gph.pkgs <- c("ggplot2")
+
+
+#Analysis
