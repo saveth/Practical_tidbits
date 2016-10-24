246347d50c40adf5745a120a778bf13651625d45
Savet Hong <savet3@gmail.com>
2016-10-23 23:17:13 -0700

    added acs packageand rmarkdown

--- Misc/pkgs.R
+++ Misc/pkgs.R
@@ -2,13 +2,16 @@
 
 #loading various forms of data
 read.pkgs <- c("readstata13", # STATA version 13 files
-              "readxl" )     # read excel files
+              "readxl",      # read excel files
+              "acs")        # census acs data files
+
 
 #data cleaning/processing
 cln.pkgs <- c("dplyr")
 
 #data visualization
-vis.pkgs <- c("flexdashboard")
+vis.pkgs <- c("rmarkdown", # generate reports
+              "flexdashboard") #dashboards
 
 #Graphics
 gph.pkgs <- c("ggplot2")
