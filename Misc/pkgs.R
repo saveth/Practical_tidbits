e091ef817501bd8427243526f6b3a307a7748dcd
Savet Hong <savet3@gmail.com>
2016-10-27 22:38:04 -0700

    Added graphics packages.

--- Misc/pkgs.R
+++ Misc/pkgs.R
@@ -14,7 +14,16 @@ vis.pkgs <- c("rmarkdown", # generate reports
               "flexdashboard") #dashboards
 
 #Graphics
-gph.pkgs <- c("ggplot2")
+gph.pkgs <- c("ggplot2",  # pretty graphics
+              "dygraphs", # interactive graphics
+              "colorRamp") # palette color option
+              
 
 
 #Analysis
+ana.pkgs <- c("zoo")  
+
+
+#install packages
+all.pkgs <- c(read.pkgs,cln.pkgs,vis.pkgs,gph.pkgs,ana.pkgs)
+install.packages(all.pkgs)
